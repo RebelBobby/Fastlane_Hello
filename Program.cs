953df7fc6440a1aa679cd16c2155380bdad072e9
RebelBobby <joseph.matulewicz@gmail.com>
2022-07-19 09:52:19 -0700

    intro class

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// namespace declaration
+namespace HelloWorldApp {
+	
+	// Class declaration
+	class program {
+		
+		// Main Method
+		static void Main(string[] args) {
+			
+			// statement
+			// printing Hello World!
+			Console.WriteLine("Hello World!");
+			
+		}
+	}
+}
