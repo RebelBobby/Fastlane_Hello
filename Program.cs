abcd4581782c5951db179daa4ee74d9bcc1d24ee
RebelBobby <joseph.matulewicz@gmail.com>
2022-07-19 12:36:34 -0700

    error

--- Program.cs
+++ Program.cs
@@ -4,21 +4,29 @@
 namespace HelloWorldApp {
 	
 	// Class declaration
-	class program {
+	class Program {
 		
 		// Main Method
 		static void Main(string[] args) {
-			
-			// statement
-			// printing Hello World!
-			Console.WriteLine("Hello World!");
-			Console.WriteLine();
 
-            //user input
-			Console.Readline();
-            
-            
-			
+			Console.WriteLine("****Basic IO ****");
+			GetUserData();
+			Console.ReadLine();
+
+			static void GetUserData()
+			{
+				Console.Write("Enter your name: ");
+				string userName = Console.Readline();
+				Console.Write("Enter your age: ");
+				string userAge = Console.ReadLine();
+
+				//Echo to the console
+				Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+			}
+
 		}
+
 	}
+
 }
+	
